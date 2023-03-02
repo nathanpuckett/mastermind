bdfe076bfc0b00d7dea775354fd6507c3ba9ecb3
nathanpuckett <nathanfpuckett@gmail.com>
2023-03-02 12:09:42 -0600

    Implement check_hits & check_blows methods

--- main.rb
+++ main.rb
@@ -6,38 +6,79 @@
 
 # Game Class
 class Game
+  @@intro = "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n" \
+            "|                             |\n" \
+            "|     M A S T E R M I N D     |\n" \
+            "|                             |\n" \
+            "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n" \
+            "\nHit: Correct Color & Position\n" \
+            "Blow: Correct Color Only\n" \
+            "\n------------------------------"
+
   def initialize
     @code = generate_code
+    @guess = []
     @turn_count = 1
+    @hits = 0
+    @blows = 0
   end
 
-  attr_reader :code, :turn_count
+  # attr_reader :code, :turn_count
 
   def play
+    puts @@intro
     loop do
-      guess = make_guess
-      if check_guess(guess)
-        puts 'You Win!'
+      @guess = make_guess
+      if check_guess
+        puts "\nYou Win! Code Guessed in #{@turn_count} Moves."
         return
       end
+      puts "\nHits: #{@hits} Blows: #{@blows}"
       return unless increment_turn
     end
   end
 
   def make_guess
-    print "Guess #{@turn_count}: "
+    print "\nGuess #{@turn_count}: "
     gets.chomp.split('').map(&:to_i)
   end
 
-  def check_guess(guess)
-    return true if guess == @code
+  def check_guess
+    return true if @guess == @code
+
+    check_hits
+    check_blows
+
+    false
+  end
+
+  def check_hits
+    @guess.each_with_index do |n, i|
+      next unless n == @code[i]
+
+      @hits += 1
+      @guess[i] = 0
+    end
+  end
+
+  def check_blows
+    @guess.each do |x|
+      @code.each do |y|
+        next unless x == y
+
+        @blows += 1
+        x = 0
+      end
+    end
   end
 
   def increment_turn
     @turn_count += 1
+    @hits = 0
+    @blows = 0
     return true unless @turn_count > 12
 
-    puts 'Game Over'
+    puts "\nGame Over. You Suck!"
   end
 
   def generate_code
